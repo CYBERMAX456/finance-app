f720b8867b581884d901b5c9fd8f1f4307fdf9e3
CYBERMAX456 <148432927+CYBERMAX456@users.noreply.github.com>
2025-06-22 19:40:51 +0530

    new file:   .gitignore 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/scenes/navbar/index.tsx 	new file:   package-lock.json 	new file:   package.json 	modified:   server/package-lock.json 	modified:   server/package.json

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import PixIcon from "@mui/icons-material/Pix";
+import PaidIcon from '@mui/icons-material/Paid';
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
 
@@ -13,7 +13,7 @@ const Navbar = (props: Props) => {
     <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
       {/* LEFT SIDE */}
       <FlexBetween gap="0.75rem">
-        <PixIcon sx={{ fontSize: "28px" }} />
+        <PaidIcon sx={{ fontSize: "28px" }} />
         <Typography variant="h4" fontSize="16px">
           Finanseer
         </Typography>
